03f12d53e26ddb1280442a18fd94af0f2c4fe56c
ZhiliangJia <zhiliangjiaai@126.com>
2023-03-13 19:15:08 +0800

    解决增加参数解析器后静态资源无法解决的问题

--- src/main/java/xyz/garbage/maven_seckill/config/WebConfig.java
+++ src/main/java/xyz/garbage/maven_seckill/config/WebConfig.java
@@ -21,6 +21,11 @@ public class WebConfig extends WebMvcConfigurationSupport {
 
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/META-INF/resources/")
+                .addResourceLocations("classpath:/resources/")
+                .addResourceLocations("classpath:/static/")
+                .addResourceLocations("classpath:/public/");
         super.addResourceHandlers(registry);
     }
 }
