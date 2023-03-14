b87d7e6dc4c2ee7d5ac9b7d66c8d44b1689aca08
ZhiliangJia <zhiliangjiaai@126.com>
2023-03-14 21:22:41 +0800

    提出公共代码，方便复用

--- src/main/java/xyz/garbage/maven_seckill/util/RedisUtil.java
+++ src/main/java/xyz/garbage/maven_seckill/util/RedisUtil.java
@@ -1,6 +1,5 @@
 package xyz.garbage.maven_seckill.util;
 
-import com.alibaba.fastjson.JSON;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,11 +20,11 @@ public class RedisUtil {
 
     public <T> T getStringValue(IRedisKey redisKey, Class<T> clazz) {
         String value = stringRedisTemplate.opsForValue().get(redisKey.getRealKey());
-        return string2Bean(value, clazz);
+        return SerializeUtil.string2Bean(value, clazz);
     }
 
     public <T> void setStringValue(IRedisKey redisKey, T value) {
-        stringRedisTemplate.opsForValue().set(redisKey.getRealKey(), beanToString(value), redisKey.getExpireSeconds(), TimeUnit.SECONDS);
+        stringRedisTemplate.opsForValue().set(redisKey.getRealKey(), SerializeUtil.beanToString(value), redisKey.getExpireSeconds(), TimeUnit.SECONDS);
     }
 
     public void deleteStringKey(IRedisKey redisKey) {
@@ -52,36 +51,5 @@ public class RedisUtil {
         }
     }
 
-    public static <T> String beanToString(T value) {
-        if (value == null) {
-            return null;
-        }
-        Class<?> clazz = value.getClass();
-        if (clazz == int.class || clazz == Integer.class) {
-            return String.valueOf(value);
-        } else if (clazz == long.class || clazz == Long.class) {
-            return String.valueOf(value);
-        } else if (clazz == String.class) {
-            return (String) value;
-        } else {
-            return JSON.toJSONString(value);
-        }
-    }
-
-
-    public static <T> T string2Bean(String str, Class<T> clazz) {
-        if (str == null || str.length() <= 0 || clazz == null) {
-            return null;
-        }
-        if (clazz == int.class || clazz == Integer.class) {
-            return (T) Integer.valueOf(str);
-        } else if (clazz == long.class || clazz == Long.class) {
-            return (T) Long.valueOf(str);
-        } else if (clazz == String.class) {
-            return (T) str;
-        } else {
-            return JSON.toJavaObject(JSON.parseObject(str), clazz);
-        }
-    }
 
 }
---
+++ src/main/java/xyz/garbage/maven_seckill/util/SerializeUtil.java
@@ -0,0 +1,37 @@
+package xyz.garbage.maven_seckill.util;
+
+import com.alibaba.fastjson.JSON;
+
+public class SerializeUtil {
+    public static <T> String beanToString(T value) {
+        if (value == null) {
+            return null;
+        }
+        Class<?> clazz = value.getClass();
+        if (clazz == int.class || clazz == Integer.class) {
+            return String.valueOf(value);
+        } else if (clazz == long.class || clazz == Long.class) {
+            return String.valueOf(value);
+        } else if (clazz == String.class) {
+            return (String) value;
+        } else {
+            return JSON.toJSONString(value);
+        }
+    }
+
+
+    public static <T> T string2Bean(String str, Class<T> clazz) {
+        if (str == null || str.length() <= 0 || clazz == null) {
+            return null;
+        }
+        if (clazz == int.class || clazz == Integer.class) {
+            return (T) Integer.valueOf(str);
+        } else if (clazz == long.class || clazz == Long.class) {
+            return (T) Long.valueOf(str);
+        } else if (clazz == String.class) {
+            return (T) str;
+        } else {
+            return JSON.toJavaObject(JSON.parseObject(str), clazz);
+        }
+    }
+}
