43296b011db91ac45f57896718562a2096b4a85e
ZhiliangJia <zhiliangjiaai@126.com>
2023-03-14 14:52:37 +0800

    修改商品详细信息在Redis中的保存的时间

--- src/main/java/xyz/garbage/maven_seckill/redis/impl/PrefixEnum.java
+++ src/main/java/xyz/garbage/maven_seckill/redis/impl/PrefixEnum.java
@@ -2,7 +2,7 @@ package xyz.garbage.maven_seckill.redis.impl;
 
 public enum PrefixEnum {
     // Good商品缓存信息
-    GOOD_LIST("GoodKey:gl_", 60), GOOD_DETAIL("GoodKey:gd_", -1), GOOD_STOCK("GoodKey:gs_", -1),
+    GOOD_LIST("GoodKey:gl_", 60), GOOD_DETAIL("GoodKey:gd_", 30), GOOD_STOCK("GoodKey:gs_", -1),
     // Order订单缓存信息
     ORDER_BY_GID("OrderKey:obg_", 120),
     // SecKillGood秒杀商品信息
